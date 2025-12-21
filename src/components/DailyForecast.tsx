b5b7c06a0bc9e9e2cfc08a030a832e6c33cfd8a9
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2025-12-21 18:22:47 +0200

    finish daily forecast component

--- src/components/DailyForecast.tsx
+++ src/components/DailyForecast.tsx
@@ -8,17 +8,25 @@ export default function DailyForecast({ }: Props) {
     const { data } = useSuspenseQuery({
         queryKey: ['weather'],
         queryFn: () => getWeather({ lat: 43, lon: 50 })
-    })
+    });
+
     return (
-        <Card title="Daily Forecast">
-            <div className='flex flex-col gap-4'>
-                {data?.daily.map((day) => (
-                    <div key={day.dt} className='flex justify-between'>
-                        <p>DATE</p>
-                        <img src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} alt="Weather icon" />
-                    </div>
-                ))}
-            </div>
+        <Card title="Daily Forecast" childrenClassName='flex flex-col gap-4'>
+            {data?.daily.map((day) => (
+                <div key={day.dt} className='flex justify-between'>
+                    <p className='w-9'>{new Date(day.dt * 1000).toLocaleDateString(undefined, {
+                        weekday: "short"
+                    })}</p>
+                    <img
+                        className='size-8'
+                        src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                        alt="Weather icon"
+                    />
+                    <p>{Math.round(day.temp.day)}°C</p>
+                    <p className='text-gray-500/75'>{Math.round(day.temp.min)}°C</p>
+                    <p className='text-gray-500/75'>{Math.round(day.temp.max)}°C</p>
+                </div>
+            ))}
         </Card>
     )
 }
