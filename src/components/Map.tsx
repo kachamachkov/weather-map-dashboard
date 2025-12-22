7da990ee19a52645666e91e56a71fe8b55808640
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2025-12-22 23:21:37 +0200

    add map layering

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,44 +2,51 @@ import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet"
 import 'leaflet/dist/leaflet.css'
 import type { Coords } from "../types"
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 type Props = {
-    coords: Coords
-    onMapClick: (lat: number, lon: number) => void
+  coords: Coords
+  onMapClick: (lat: number, lon: number) => void
 }
 
 export default function Map({ coords, onMapClick }: Props) {
-    const { lat, lon } = coords;
+  const { lat, lon } = coords;
 
-    return (
-        <MapContainer
-            center={[lat, lon]}
-            zoom={5}
-            style={{ width: '700px', height: '500px' }}
-        >
-            <MapClick onMapClick={onMapClick} coords={coords} />
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[lat, lon]} />
-        </MapContainer>
-    )
+  return (
+    <MapContainer
+      center={[lat, lon]}
+      zoom={5}
+      style={{ width: '700px', height: '500px' }}
+    >
+      <MapClick onMapClick={onMapClick} coords={coords} />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <TileLayer
+        url={`https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid=${API_KEY}`}
+
+      />
+      <Marker position={[lat, lon]} />
+    </MapContainer>
+  )
 }
 
 function MapClick({
-    onMapClick,
-    coords
+  onMapClick,
+  coords
 }: {
-    onMapClick: (lat: number, lon: number) => void
-    coords: Coords
+  onMapClick: (lat: number, lon: number) => void
+  coords: Coords
 }) {
-    const map = useMap();
-    map.panTo([coords.lat, coords.lon]);
+  const map = useMap();
+  map.panTo([coords.lat, coords.lon]);
 
-    map.on('click', (e) => {
-        const { lat, lng } = e.latlng;
-        onMapClick(lat, lng);
-    })
+  map.on('click', (e) => {
+    const { lat, lng } = e.latlng;
+    onMapClick(lat, lng);
+  })
 
-    return null;
+  return null;
 }
