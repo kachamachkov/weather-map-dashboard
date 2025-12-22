f61ad33906d159de05b357c90f969d8cddc52a71
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2025-12-22 19:50:43 +0200

    fix bugs, data being undefined during flight

--- src/schemas/weatherSchema.ts
+++ src/schemas/weatherSchema.ts
@@ -16,10 +16,10 @@ export const weatherSchema = z.object({
     dew_point: z.number(),
     uvi: z.number(),
     clouds: z.number(),
-    visibility: z.number(),
+    visibility: z.number().optional(),
     wind_speed: z.number(),
     wind_deg: z.number(),
-    wind_gust: z.number(),
+    wind_gust: z.number().optional(),
     weather: z.array(
       z.object({
         id: z.number(),
@@ -39,10 +39,10 @@ export const weatherSchema = z.object({
       dew_point: z.number(),
       uvi: z.number(),
       clouds: z.number(),
-      visibility: z.number(),
+      visibility: z.number().optional(),
       wind_speed: z.number(),
       wind_deg: z.number(),
-      wind_gust: z.number(),
+      wind_gust: z.number().optional(),
       weather: z.array(
         z.object({
           id: z.number(),
