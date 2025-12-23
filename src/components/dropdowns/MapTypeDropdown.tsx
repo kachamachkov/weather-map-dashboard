1e7bfa8565eed1d41311bed65d587fe0c3f5537f
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2025-12-23 18:42:25 +0200

    add map type component

---
+++ src/components/dropdowns/MapTypeDropdown.tsx
@@ -0,0 +1,35 @@
+import type { Dispatch, SetStateAction } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
+
+type Props = {
+  mapType: string,
+  setMapType: Dispatch<SetStateAction<string>>
+}
+
+export default function MapTypeDropdown({ mapType, setMapType }: Props) {
+  return (
+    <Select
+      value={mapType}
+      onValueChange={(value) => setMapType(value)}
+    >
+      <SelectTrigger className="w-45">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent className="z-1001">
+        {types.map((mapType) => (
+          <SelectItem key={mapType} value={mapType} className="capitalize" >
+            {mapType.split('_')[0]}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  )
+}
+
+const types = [
+  "clouds_new",
+  "precipitation_new",
+  "pressure_new",
+  "wind_new",
+  "temp_new ",
+];
