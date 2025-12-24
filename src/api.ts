3a50b7e1dc9d2d0045a522f86eeeb0c86b741ef8
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2025-12-24 16:13:45 +0200

    add air pollution fetch function

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,16 @@
 import { weatherSchema } from "./schemas/weatherSchema";
 import { geocodeSchema } from "./schemas/geocodeSchema";
+import { airPollutionSchema } from "./schemas/airPollutionSchema.ts";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
+export async function getWeather({
+  lat,
+  lon,
+}: {
+  lat: number
+  lon: number
+}) {
   try {
     const res = await fetch(
       `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=${API_KEY}`
@@ -27,4 +34,18 @@ export async function getGeocode(location: string) {
   } catch (err) {
     console.error(err);
   }
+}
+
+export async function getAirPollution({
+  lat,
+  lon,
+}: {
+  lat: number
+  lon: number
+}) {
+  const res = await fetch(
+    `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  )
+  const data = await res.json()
+  return airPollutionSchema.parse(data)
 }
